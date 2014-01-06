1d7d7fe3ffd2a2a728428138039e53d337f463af
David McGillicuddy <dmcgill9071@gmail.com>
2014-01-06 16:01:37 +0000

    played around with strictness a little; made it a library instead of an exe

--- Knapsack.hs
+++ Knapsack.hs
@@ -1,50 +1,64 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, BangPatterns #-}
 
---module Knapsack where
+module Knapsack (solve) where
 
-import Control.Arrow (second, (***))
-import Control.Applicative ((<$>))
+import           Control.Arrow       (second, (***))
 import qualified Data.IntMap.Strict as I
-import Data.Maybe (catMaybes)
-import Data.Ord (comparing)
-import System.Random
+import           Data.List           (foldl')
+import           Data.Maybe          (catMaybes)
+import           Data.Ord            (comparing)
+import           System.Random
 import qualified Data.Vector as V
 
-{-
-knapsackScaled.genI.eachPair Main     44.1   33.8
-knapsackScaled.genI          Main     32.6   59.7
-safeMaximum                  Main     10.4    5.2
-compare                      Main     10.2    0.0
-egRandom                     Main      2.2    0.9
+{- TODO:
+talk about
+    lazy vs strict in Solution
+    making safeMaximum strict basically eliminiated all the Solutions on the stack
+use criteron to compare the lazy vs strict versions on the SAME problem
+
+ALLOCATION:
+    Int
+    Integer
+    (,)
+    (*)
+
+COST CENTRE                  MODULE  %time %alloc
+
+knapsackScaled.genI.eachPair Main     45.9   30.1
+knapsackScaled.genI          Main     38.3   67.9
+compare                      Main      6.7    0.0
+safeMaximum                  Main      5.3    0.0
+egRandom                     Main      3.0    1.4
 -}
 
 type Selection = I.IntMap Int
 newtype Value = V {unV :: Int} deriving (Eq, Ord, Num, Real, Enum, Integral, Show)
 newtype Weight = W {unW :: Int} deriving (Eq, Ord, Num, Real, Enum, Integral, Show)
 
-data Solution = Solution {selection :: Selection, totalWeight :: !Weight, totalValue :: !Value}
+data Solution = Solution {selection :: !Selection, totalValue :: !Value, totalWeight :: !Weight}
     deriving (Eq, Show)
 
 emptySoln = Solution I.empty 0 0
 
 instance Ord Solution where
     compare = comparing totalValue
-
-main = print =<< egRandom 10000
+{-
+main = print =<< egRandom (10000 :: Int)
 
 egRandom n = do
-    (cap:xs) <- randomRs (1,1000) <$> newStdGen
+    (cap:xs) <- randomRs (1,1000 :: Int) `fmap` newStdGen
     let vws = take n $ pair xs
     return (solve vws cap)
     where
     pair :: [a] -> [(a,a)]
     pair (x1:x2:xs) = (x1,x2) : pair xs
     pair _ = []
-
+-}
 -- | Given a list of (Value, Weight) pairs, maximises the total value
 --   while keeping the total weight under the capacity.
-solve :: [(Int, Int)] -> Int -> Solution
-solve vws cap = knapsackNative (V.fromList $ map (V *** W) vws) (W cap)
+solve :: [(Int, Int)] -> Int -> (I.IntMap Int, Int, Int)
+solve vws cap = (s, v, w)
+    where Solution s (V v) (W w) = knapsackNative (V.fromList $ map (V *** W) vws) (W cap)
 
 knapsackNative :: V.Vector (Value, Weight) -> Weight -> Solution
 knapsackNative vws cap = unscale $ knapsackScaled (V.map (second scale) vws) (scale cap)
@@ -52,23 +66,23 @@ knapsackNative vws cap = unscale $ knapsackScaled (V.map (second scale) vws) (sc
     -- the gcd of the weights
     gcdW = V.foldl1' gcd $ V.map snd vws
     scale x = div x gcdW
-    unscale (Solution s w v) = Solution s (w*gcdW) v
+    unscale (Solution s v w) = Solution s v (w*gcdW)
 
 knapsackScaled :: V.Vector (Value, Weight) -> Weight -> Solution
 knapsackScaled vws (W cap) = solns V.! cap
     where
+    -- a vector of the best solution at each (scaled) weight
     solns :: V.Vector Solution
     solns = V.generate (cap+1) genI
 
     genI :: Int -> Solution
     genI 0 = emptySoln
-    genI i = safeMaximum $ catMaybes $ V.toList $ V.imap eachPair vws
+    genI !i = safeMaximum $ catMaybes $ V.toList $ V.imap eachPair vws
         where
         eachPair :: Int -> (Value, Weight) -> Maybe Solution
-        eachPair j (v, w) | unW w <= i = case solns V.! (i - unW w) of
-            Solution s' w' v' -> Just $ Solution (I.insertWith (+) j 1 s') (w' + w) (v' + v)
+        eachPair !j (v, w) | unW w <= i = case solns V.! (i - unW w) of
+            Solution s' v' w' -> Just $ Solution (I.insertWith (+) j 1 s') (v' + v) (w' + w)
         eachPair _ _ = Nothing
 
 safeMaximum :: [Solution] -> Solution
-safeMaximum [] = emptySoln
-safeMaximum xs = maximum xs
+safeMaximum = foldl' max emptySoln
