5bf63a051da4b9ecb86f039fe36439e27d3da76a
David McGillicuddy <dmcgill9071@gmail.com>
2014-01-30 12:31:50 +0000

    changed Main to be a little more clear and to accept the new problem format.

--- Main.hs
+++ Main.hs
@@ -2,6 +2,13 @@ module Main where
 
 import Knapsack
 
+{-
+gcd = 1
+selection = fromList [(6525,40),(9967,30)]
+total value = 69360
+total weight = 7040
+-}
+
 main = do
     (cap, vws) <- readProblem "test_problem_1.data"
     print $ solve vws cap
@@ -9,10 +16,10 @@ main = do
 readProblem :: String -> IO (Int, [(Int,Int)])
 readProblem filename = do
     file <- readFile filename
-    let (capStr:xsStr) = lines file
-    return (read capStr, map readVW xsStr)
+    let ([cap, _]:xss) = map (map read . words) (lines file)
+    return (cap, map getVW xss)
 
     where
-    readVW :: String -> (Int,Int)
-    readVW str = (read vStr, read wStr)
-        where [vStr, wStr] = words str
+    getVW :: [Int] -> (Int,Int)
+    getVW [v, w] | v >= 0 && w > 0 = (v,w)
+                 | otherwise = error "assertion 'v >= 0 && w > 0' failed in getVW"
