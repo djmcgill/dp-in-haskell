688b1866dd2d8a9d4d7c3c91fb20e0614d5f187e
Swooshed <dmcgill9071@gmail.com>
2014-03-03 11:09:59 +0000

    added in the definition of gcd to the haskell example

--- patat/code/gcds.hs
+++ patat/code/gcds.hs
@@ -1,2 +1,9 @@
+import Prelude hiding (gcd)
+
+gcd :: Int -> Int -> Int
+gcd 1 _ = 1
+gcd 0 b = b
+gcd a b = gcd (b `mod` a) a
+
 gcds :: Int -> [Int] -> Int
 gcds capacity weights = foldr1 gcd (capacity:weights)
